72b923eccd6813f2116d6737b524523679241127
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-07-26 00:46:37 +0200

    set up

---
+++ app/account/page.js
@@ -0,0 +1,6 @@
+export const metadata = {
+  title: 'Account',
+};
+export default function AccountPage() {
+  return <div>hello from account page</div>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Cabins',
+};
+
+export default function CabinsPage() {
+  return (
+    <>
+      <h1>hello from cabins page</h1>
+    </>
+  );
+}
