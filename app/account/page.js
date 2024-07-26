5044001e0217ac5dd4f54110f743aebe6faad8ed
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-07-26 12:55:57 +0200

    basic layout before setting up db

--- app/account/page.js
+++ app/account/page.js
@@ -1,6 +1,11 @@
 export const metadata = {
-  title: 'Account',
+  title: 'Guest area',
 };
+
 export default function AccountPage() {
-  return <div>hello from account page</div>;
+  return (
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome, name
+    </h2>
+  );
 }
