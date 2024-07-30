044fa07aef9939f50ca0f7476ca5ec5383abaf96
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-07-30 15:40:03 +0200

    add datepicker + reservation form + data fetching + suspense

---
+++ app/_components/Reservation.js
@@ -0,0 +1,22 @@
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
+
+export default async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    // getBookedDatesByCabinId(params.cabinId),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
